81e48e850663f0055c865a340d69a51d6c356364
waltermilcoff <waltermilcoff@gmail.com>
2022-09-16 12:46:31 -0300

    Comenzando un nuevo proyecto mediante Spring Initializr

---
+++ src/main/java/com/waltermilcoff/springbootaplicacion/SpringbootaplicacionApplication.java
@@ -0,0 +1,13 @@
+package com.waltermilcoff.springbootaplicacion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootaplicacionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootaplicacionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/waltermilcoff/springbootaplicacion/SpringbootaplicacionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.waltermilcoff.springbootaplicacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootaplicacionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
